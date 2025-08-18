f809e55ae75ef19da8e7b756f0f4c1614fa52f77
Arnab Bera <arnabbera24122002@gmail.com>
2025-08-18 20:25:55 +0530

    Time: 15 ms (12.94%), Space: 23.2 MB (17.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        ## At first remove all the special charecter and space , also convert into lower charecter
-        new=("".join(char for char in s if char.isalnum())).lower()
-        return new==new[::-1]
+        # ## At first remove all the special charecter and space , also convert into lower charecter
+        # new=("".join(char for char in s if char.isalnum())).lower()
+        # return new==new[::-1]
+        lst=[]
+        for i in range(len(s)):
+            if s[i].isalnum():
+                lst.append(s[i].lower())
+        newStr="".join(lst)
+        return newStr==newStr[::-1]
